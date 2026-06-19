86c67eda74f3d7456e9bea15b5f896c4d2651c7b
hosonhao2k17 <hosonhao2004@icloud.com>
2026-06-19 14:38:20 +0700

    chore: error response

---
+++ common/dto/error_res_dto.go
@@ -0,0 +1,46 @@
+package dto
+
+import (
+	"haoflowcake/common/constant"
+	"time"
+)
+
+type ErrorResDto struct {
+	Message    string             `json:"message"`
+	StatusCode int                `json:"status_code"`
+	Timestamp  time.Time          `json:"timestamp"`
+	ErrorCode  constant.ErrorCode `json:"error_code"`
+	Details    []ErrorDetail      `json:"details,omitempty"`
+}
+
+type ErrorDetail struct {
+	Property string `json:"property"`
+	Code     string `json:"code"`
+	Message  string `json:"message"`
+}
+
+func (err *ErrorResDto) Error() string {
+	return err.Message
+}
+
+func NewErrorResponse(errCode constant.ErrorCode) *ErrorResDto {
+	currentTime := time.Now()
+	return &ErrorResDto{
+		ErrorCode:  errCode,
+		Message:    constant.MapErrorCode[errCode].Message,
+		StatusCode: constant.MapErrorCode[errCode].StatusCode,
+		Timestamp:  currentTime,
+	}
+}
+
+func NewFullErrorResponse(errCode constant.ErrorCode, details []ErrorDetail) *ErrorResDto {
+
+	currentTime := time.Now()
+	return &ErrorResDto{
+		ErrorCode:  errCode,
+		Message:    constant.MapErrorCode[errCode].Message,
+		StatusCode: constant.MapErrorCode[errCode].StatusCode,
+		Timestamp:  currentTime,
+		Details:    details,
+	}
+}
